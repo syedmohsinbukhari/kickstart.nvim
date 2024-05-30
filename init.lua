83556527ecf32043d8ea6462a853499652549569
Syed Mohsin Bukhari <syed.mohsin-bukhari@chaos.com>
2024-05-30 23:43:51 +0200

    Set scrolloff to 3
    Signed-off-by: Syed Mohsin Bukhari <syed.mohsin-bukhari@chaos.com>

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 3
 
 vim.opt.wrap = false
 
